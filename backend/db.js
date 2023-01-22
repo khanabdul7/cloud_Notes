e36e5ef19b580eeb391fe0d38bbd313ffb639e8c
Abdul khan <abdulmatania@gmail.com>
2023-01-22 12:54:23 +0530

    Initial Commit

---
+++ backend/db.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const mongoURI = "mongodb://localhost:27017/"
+
+const connectToMongo = () => {
+    mongoose.connect(mongoURI, ()=>{
+        console.log("Connected to Mongo Succesfully");
+    })
+}
+
+module.exports = connectToMongo;
